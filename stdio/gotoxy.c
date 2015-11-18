4ffc75ce243a5ac8a47f6a8a87450b031b9201f1
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-19 06:59:12 +0800

    Create gotoxy.c

---
+++ stdio/gotoxy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void gotoxy(int x, int y)
+{
+	__asm__ __volatile__
+	(
+		"mov $0x02, %%ah\n"
+		"mov %1, %%dh\n"
+		"mov %2, %%dl\n"
+		"int $0x10\n"
+		:: "b"(__video_attributes) "m"(x), "m"(y)
+	);
+}
