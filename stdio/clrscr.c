34ccf0fbc696419ac19149ee50ba267e37b3677e
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-19 07:01:49 +0800

    Create clrscr.c

---
+++ stdio/clrscr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void clrscr(void)
+{
+	__asm__ __volatile__
+	(
+		"int $0x10\n"
+		:: "a"(0x0600), "b"(__video_attributes), "c"(0x0000), "d"(0x1950)
+	);
+
+	gotoxy(0, 0);
+}
