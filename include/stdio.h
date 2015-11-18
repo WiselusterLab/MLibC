d8f16b39de71fba1d4ff279dfddab4f821cda8fb
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-18 22:34:53 +0800

    Create stdio.h

---
+++ include/stdio.h
@@ -0,0 +1,9 @@
+#ifndef _MLIBC_STDIO_H_INCLUDED_
+#define _MLIBC_STDIO_H_INCLUDED_ 1
+
+#include <mlibc.h>
+
+__C int putchar(int ch);
+__C int puts(const char *str);
+
+#endif /* _MLIBC_STDIO_H_INCLUDED_ */
