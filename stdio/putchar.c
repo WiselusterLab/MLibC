3bd5462273649c4dfa02e7a94363ae2acb188319
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-19 19:18:44 +0800

    Update putchar.c

--- stdio/putchar.c
+++ stdio/putchar.c
@@ -4,11 +4,14 @@ int putchar(int ch)
 {
 	__asm__ __volatile__
 	(
+		"push %%edx\n"
 		"push %%ebp\n"
+		"mov %3, %%edx\n"
 		"lea %4, %%ebp\n"
 		"int $0x10\n"
+		"pop %%edx\n"
 		"pop %%ebp\n"
-		:: "a"(0x1301), "b"(__video_attributes), "c"(0x0001), "d"(getxy()), "m"(ch)
+		:: "a"(0x1301), "b"(__video_attributes), "c"(0x0001), "i"(getxy()), "m"(ch)
 	);
 
 	return ch;
