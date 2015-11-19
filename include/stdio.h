985f96bfff8e817fefcf439f68eb085cff863ba6
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-19 20:10:15 +0800

    Update stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -5,17 +5,18 @@
 
 __BEGIN_DECLS
 
-/* Standard C I/O Functions */
+/* Standard C I/O functions */
 extern int putchar(int ch);
 extern int puts(const char *str);
 
-/* Non-Standard C I/O Functions */
+/* Non-standard C I/O functions */
 extern void clrscr(void);
 extern dword getxy(void);
 extern void gotoxy(dword x, dword y);
 
 __END_DECLS
 
+/* Video attribute data */
 extern word __video_attributes __MERGE;
 
 #endif /* _MLIBC_STDIO_H_INCLUDED_ */
