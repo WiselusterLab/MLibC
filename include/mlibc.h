47cd724f4bca67dffa900518c14ced3ac8e11226
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 22:15:28 +0800

    Update mlibc.h

--- include/mlibc.h
+++ include/mlibc.h
@@ -1,6 +1,7 @@
 #ifndef __MLIBC__
 #define __MLIBC__ 1
 
-#define __MLIBC_VERSION__ 201511
+#define __MLIBC_VERSION__ MLIBC_VERSION_SET(0, 0, 2, 0)
+#define MLIBC_VERSION_SET(major, minor, patch, small) (((major) << 0x18) | ((minor) << 0x10) | ((patch) << 0x08) | (small))
 
 #endif /* __MLIBC__ */
