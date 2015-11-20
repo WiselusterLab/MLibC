60e620a953fc08e7f5167dce59d037e6cb18689f
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 22:41:50 +0800

    Update global.c

--- stdio/global.c
+++ stdio/global.c
@@ -1,3 +1,3 @@
 #include <stdio.h>
 
-int __mlibc_vidattrr __attribute__((section(".text"))) = BIOS_VIDEO_ATTRIB_DEFAULT;
+int __mlibc_vidattr __attribute__((section(".text"))) = BIOS_VIDEO_ATTRIB_DEFAULT;
