a613404cf8b69a35968503ad1db1e581850ef03b
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 22:34:08 +0800

    Update gotoxy.c

--- stdio/gotoxy.c
+++ stdio/gotoxy.c
@@ -5,8 +5,8 @@ void gotoxy(int x, int y)
 	__asm__ __volatile__
 	(
 		"mov $0x02, %%ah\n"
-		"mov %0, %%dh\n"
-		"mov %1, %%dl\n"
+		"mov %1, %%dh\n"
+		"mov %2, %%dl\n"
 		"int $0x10\n"
 		:: "b"(__mlibc_vidattr), "m"(x), "m"(y)
 	);
