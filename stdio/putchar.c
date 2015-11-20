2daefbfecc2517f502e03b18d382e4ce4a0b7e20
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 22:18:29 +0800

    Update putchar.c

--- stdio/putchar.c
+++ stdio/putchar.c
@@ -8,7 +8,7 @@ int putchar(int ch)
 		"lea %4, %%ebp\n"
 		"int $0x10\n"
 		"pop %%ebp\n"
-		:: "a"(0x1301), "b"(__mlibc_bios_video_attributes), "c"(0x0001), "d"(getxy()), "m"((&ch)[1])
+		:: "a"(0x1301), "c"(0x0001), "d"(getxy()), "m"((&ch)[1])
 	);
 
 	return ch;
