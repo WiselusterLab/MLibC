5e4ef4f8698dad2e2b1ca4abfa57ebb7d1de1e7e
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 21:59:06 +0800

    Update _start.c

--- crt/_start.c
+++ crt/_start.c
@@ -3,6 +3,14 @@ extern int main(void);
 void _start(void)
 {
 	main();
+#ifdef __DOS__
+	__asm __volatile__
+	(
+		"mov $0x4C, %ah\n"
+		"int $0x21\n"
+	);
+#else
 	while (1)
 		__asm__ __volatile__ ("hlt\n");
+#endif
 }
