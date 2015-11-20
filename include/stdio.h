08312b859ba49570845b8d567ab0537469196d76
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 10:17:21 +0800

    Update stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -11,34 +11,15 @@ extern int puts(const char *str);
 
 /* Non-standard C I/O functions */
 extern void clrscr(void);
-extern int getxy(void);
-extern void gotoxy(int x, int y);
+extern word getxy(void);
+extern void gotoxy(byte x, byte y);
 
 __END_DECLS
 
 /* Video attribute data */
-extern int __video_attributes __MERGE;
+extern word __mlibc_video_attributes __MERGE;
 
-/* Video attribute constants */
-#define BIOS_VIDEO_ATTRIBUTES(page, color) (((page) << 0x08) | (color))
-#define BIOS_PAGE_CURRENT 0x00 
-#define BIOS_COLOR(fore, back) ((fore) | ((back) << 0x04))
-#define BIOS_COLOR_ORIGINAL BIOS_COLOR(BIOS_COLOR_LIGHT_GRAY, BIOS_COLOR_BLACK)
-#define BIOS_COLOR_BLACK 0x00
-#define BIOS_COLOR_BLUE 0x01
-#define BIOS_COLOR_GREEN 0x02
-#define BIOS_COLOR_CYAN 0x03
-#define BIOS_COLOR_RED 0x04
-#define BIOS_COLOR_MAGNETA 0x05
-#define BIOS_COLOR_BROWN 0x06
-#define BIOS_COLOR_LIGHT_GRAY 0x07
-#define BIOS_COLOR_DARK_GRAY 0x08
-#define BIOS_COLOR_LIGHT_BLUE 0x09
-#define BIOS_COLOR_LIGHT_GREEN 0x0A
-#define BIOS_COLOR_LIGHT_CYAN 0x0B
-#define BIOS_COLOR_LIGHT_RED 0x0C
-#define BIOS_COLOR_LIGHT_MAGNETA 0x0D
-#define BIOS_COLOR_YELLOW 0x0E
-#define BIOS_COLOR_WHITE 0x0F
+/* Header for BIOS video macros */
+#include <bios.h>
 
 #endif /* _MLIBC_STDIO_H_INCLUDED_ */
