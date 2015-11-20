9f6eb610121ee5396ee3a0c22a047f0f8e3a1087
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 10:23:13 +0800

    Update clrscr.c

--- stdio/clrscr.c
+++ stdio/clrscr.c
@@ -6,7 +6,7 @@ void clrscr(void)
 	(
 		"xchg %%bl, %%bh\n"
 		"int $0x10\n"
-		:: "a"(0x0600), "b"(__video_attributes), "c"(0x0000), "d"(0x1950)
+		:: "a"(0x0600), "b"(__mlibc_bios_video_attributes), "c"(0x0000), "d"(0x1950)
 	);
 
 	gotoxy(0, 0);
