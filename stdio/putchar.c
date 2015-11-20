18ea6ad29d391d14332f0fd80f7f1b643db8b62a
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 22:36:19 +0800

    Update putchar.c

--- stdio/putchar.c
+++ stdio/putchar.c
@@ -5,10 +5,10 @@ int putchar(int ch)
 	__asm__ __volatile__
 	(
 		"push %%ebp\n"
-		"lea %3, %%ebp\n"
+		"lea %4, %%ebp\n"
 		"int $0x10\n"
 		"pop %%ebp\n"
-		:: "a"(0x1301), "c"(0x0001), "d"(getxy()), "m"((&ch)[1])
+		:: "a"(0x1301), "b"(__mlibc_vidattr), "c"(0x0001), "d"(getxy()), "m"((&ch)[1])
 	);
 
 	return ch;
