8c87b016a78b089af201d64cf0053956b85409dc
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-20 19:22:49 +0800

    Create math.h

---
+++ include/math.h
@@ -0,0 +1,12 @@
+#ifndef _MLIBC_MATH_H_INCLUDED_
+#define _MLIBC_MATH_H_INCLUDED_ 1
+
+#include <stddef.h>
+
+__BEGIN_DECLS
+
+int abs(int val);
+
+__END_DECLS
+
+#endif /* _MLIBC_MATH_H_INCLUDED_ */
