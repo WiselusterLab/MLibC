ee26e6cecbf4247e06cd49cdb2d43c0c8f25c822
Wiseluster <Wiseluster@users.noreply.github.com>
2015-11-21 06:54:12 +0800

    Update stdio.h

--- include/stdio.h
+++ include/stdio.h
@@ -9,16 +9,6 @@ __BEGIN_DECLS
 extern int putchar(int ch);
 extern int puts(const char *str);
 
-/* BIOS based I/O functions */
-extern void clrscr(void);
-extern int getxy(void);
-extern void gotoxy(int x, int y);
-
 __END_DECLS
-
-extern int __mlibc_vidattr;
-
-/* Header for BIOS video macros */
-#include <bios.h>
 
 #endif /* _MLIBC_STDIO_H_INCLUDED_ */
